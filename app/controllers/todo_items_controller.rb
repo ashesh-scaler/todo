4271779e6bb18b522ac8c7ccecc38913a77a00b4
AsheshDubey <asheshd2001@gmail.com>
2023-01-03 17:33:58 +0530

    Updated Style

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,12 +10,13 @@ class TodoItemsController < ApplicationController
     def destroy
         @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.destroy
-        flash[:success] = "Todo List item was deleted."
+            flash[:success] = "Todo List item was deleted."
         else
-        flash[:error] = "Todo List item could not be deleted."
+            flash[:error] = "Todo List item could not be deleted."
         end
         redirect_to @todo_list 
     end
+    
     def complete
         @todo_item.update_attribute(:completed_at, Time.now)
         redirect_to @todo_list, notice: "Todo item completed"
